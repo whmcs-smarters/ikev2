3a1cd87b56394678602457731a1044114f032d20
Amanpreet Singh <support@whmcssmarters.com>
2020-01-04 14:18:46 +0530

    Improved

--- install-ikev2.sh
+++ install-ikev2.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
-#
-# Script for automatic setup of an IPsec VPN server on Ubuntu LTS 18.04 64 bit
-
-# Works on any dedicated server or virtual private server (VPS) except OpenVZ.
-#
-# DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC!
-
-# Writtne by WHMCS-Smarters ( www.whmcssmarters.com) 
-
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 SYS_DT=$(date +%F-%T)
 
@@ -16,48 +6,54 @@ exiterr2() { exiterr "'apt-get install' failed."; }
 conf_bk() { /bin/cp -f "$1" "$1.old-$SYS_DT" 2>/dev/null; }
 bigecho() { echo; echo "## $1"; echo; }
 
-echo "Installing Started ...."
-echo " Updating the Server First ...."
+PUBLIC_IP=$(dig @resolver1.opendns.com -t A -4 myip.opendns.com +short)
+[ -z "$PUBLIC_IP" ] && PUBLIC_IP=$(wget -t 3 -T 15 -qO- http://ipv4.icanhazip.com)
+
+echo " Public IP Address: " printf '%s\n' "$PUBLIC_IP"
+
+bigecho "VPN setup in progress... Please be patient."
 
 sudo apt update -y
-echo " Server  Updated Successfully "
-echo " *********"
-echo " Installing StrongSwan and Strongwan-pki"s
-
-sudo apt install strongswan strongswan-pki -y
+ 
+sudo apt install strongswan strongswan-pki -yq || exiterr2
 
 echo " Strongswan Installed " 
-echo "***********"
-echo " Making Directories for Certs files " 
-mkdir -p ~/pki/{cacerts,certs,private}
 
-echo" Directories Created Successfully " 
+count=0
+APT_LK=/var/lib/apt/lists/lock
+PKG_LK=/var/lib/dpkg/lock
+while fuser "$APT_LK" "$PKG_LK" >/dev/null 2>&1 \
+  || lsof "$APT_LK" >/dev/null 2>&1 || lsof "$PKG_LK" >/dev/null 2>&1; do
+  [ "$count" = "0" ] && bigecho "Waiting for apt to be available..."
+  [ "$count" -ge "60" ] && exiterr "Could not get apt/dpkg lock."
+  count=$((count+1))
+  printf '%s' '.'
+  sleep 3
+done
+
+echo "***********"
+
+echo " Making Directories for Certs files " 
+
+if [ -d "~/pki/" ] 
+then
+    echo "Directory exists and removed "
+rm -r ~/pki/
+ 
+else
+    echo "Message: Directory ~/pki/ does not exists,So creating..."
+fi
+
+mkdir -p ~/pki/{cacerts,certs,private} || exiterr " Directories not created "
 
 chmod 700 ~/pki
-echo "Now that we have a directory structure to store everything, we can generate a root key. This will be a 4096-bit RSA key that will be used to sign our root certificate authority."
 
 ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
-
-echo " *********** " 
-
-echo "Now that we have a key, we can move on to creating our root certificate authority, using the key to sign the root certificate"
-
 
 ipsec pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem \
     --type rsa --dn "CN=VPN root CA" --outform pem > ~/pki/cacerts/ca-cert.pem
 
-echo " We’ll now create a certificate and key for the VPN server. This certificate will allow the client to verify the server’s authenticity using the CA certificate we just generated."
-
 ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
-echo " *** Done **** " 
-
-echo" Getting Public IP , if you can't see the IP now, you have to update it manually " 
-
-PUBLIC_IP=$(dig @resolver1.opendns.com -t A -4 myip.opendns.com +short)
-[ -z "$PUBLIC_IP" ] && PUBLIC_IP=$(wget -t 3 -T 15 -qO- http://ipv4.icanhazip.com)
-echo " Public IP Address: " printf '%s\n' "$PUBLIC_IP"
-
-echo " Now, create and sign the VPN server certificate with the certificate authority’s key you created in the previous step"
 
 ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
     | ipsec pki --issue --lifetime 1825 \
@@ -67,18 +63,23 @@ ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
         --flag serverAuth --flag ikeIntermediate --outform pem \
     >  ~/pki/certs/server-cert.pem
 
-echo "Now that we’ve generated all of the TLS/SSL files StrongSwan needs, we can move the files into place in the /etc/ipsec.d "
-sudo cp -r ~/pki/* /etc/ipsec.d/
-echo " Copied " 
+cp -r ~/pki/* /etc/ipsec.d/
 
-echo " ********** " 
-echo "StrongSwan has a default configuration file with some examples, but we will have to do most of the configuration ourselves. Let’s back up the file for reference before starting from scratch:"
+bigecho "Installing packages required for setup..."
 
-sudo mv /etc/ipsec.conf{,.original}
+apt-get -yq install wget dnsutils openssl \
+  iptables iproute2 gawk grep sed net-tools || exiterr2
+
+
+# Create IPsec config
+#conf_bk "/etc/ipsec.conf"
 
 if [[ -e /etc/ipsec.conf ]]; then
+
 rm /etc/ipsec.conf
+
 echo "Remooved ipsec.conf existing file"
+
 fi
 
 cat >> /etc/ipsec.conf <<EOF
@@ -127,6 +128,9 @@ ipsec restart
 
 bigecho "Installion Done" 
 
+ca_cert = $(cat /etc/ipsec.d/cacerts/ca-cert.pem)
+echo " Username :  test"
+echo " Password : test123"
+echo " Certificate is " 
 
- 
-
+echo $ca_cert;
