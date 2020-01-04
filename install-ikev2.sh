b2cf327f1ddc9ec0fbb3f35d1fa8e56697f664fd
Amanpreet Singh <support@whmcssmarters.com>
2020-01-04 15:10:27 +0530

    Completed

--- install-ikev2.sh
+++ install-ikev2.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+#
+# Script for automatic setup of an IPsec VPN server on Ubuntu LTS and Debian.
+# Works on any dedicated server or virtual private server (VPS) except OpenVZ.
+#
+# DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC!
+
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 SYS_DT=$(date +%F-%T)
 
@@ -128,7 +135,7 @@ ipsec restart
 
 bigecho "Installion Done" 
 
-ca_cert = $(cat /etc/ipsec.d/cacerts/ca-cert.pem)
+ca_cert=$(cat /etc/ipsec.d/cacerts/ca-cert.pem)
 echo " Username :  test"
 echo " Password : test123"
 echo " Certificate is " 
